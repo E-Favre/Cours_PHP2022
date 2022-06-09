d3bde4ce76b3cfbed722d3cda4d6ab7dd25376e0
E-Favre <68663660+E-Favre@users.noreply.github.com>
2022-06-09 12:07:52 +0200

    Corrections
    Quelques corrections sur method.php

--- 01_Introduction/Introduction.php
+++ 01_Introduction/Introduction.php
@@ -10,6 +10,7 @@
 
     <!-- Bootstrap CSS v5.2.0-beta1 -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    
     <!-- font google -->
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Bad+Script&display=swap" rel="stylesheet">
--- inc/functions.php
+++ inc/functions.php
@@ -1,24 +1,23 @@
 <?php
     //une première fonction
-    function minmap(){
+    function minpap(){
         echo"<p class=\"lead\">Minute papillon !</p>";
     }
-
     //fonction pour afficher le jour de la semaine
     function quelJour(){
         setlocale(LC_ALL, 'fr_FR');
-        echo "<p class=\"alert alert-primary w-50 mx-auto text-center\">Nous sommes " . strftime('%A %e %B %Y') . "</p>";
-        echo "<p class=\"alert alert-primary w-50 mx-auto text-center\">We are " . strftime('l j F Y') . "</p>";
+        echo "<p class=\"alert alert-primary w-50 mx-auto text-center\">Nous sommes " . strftime('%A %e %B %Y') . ".</p>";
+        echo "<p class=\"alert alert-primary w-50 mx-auto text-center\">We are " . date("l j F Y") . ".</p>";
+
     }
     // création d'une fonction qui var_dump une variable : très utile pour un tableau
-    function jevar_dump($mavariable){//la fonction nommée avec son paramètre : variable à modifier pour celle qu'on veut afficher
-        echo "<small class=\"bg-success text-white p-2\">var_dump :<pre class=\"alert alert-success w-75\">";
+    function jevar_dump($mavariable){// la fonction nommée avec son paramètre : une variable qu'on va pouvoir modifier pour mettre le nom de celle qu'on veut afficher
+        echo "<small class=\"bg-success text-white mx-auto mt-3 \">var_dump :<pre class=\"alert alert-success w-50 \">";
         var_dump($mavariable);
         echo "</pre>";
     }
-
     function jeprint_r($mavariable){
-        echo "<small class=\"bg-success text-white p-2\">var_dump :<pre class=\"alert alert-success w-75\">";
+        echo "<small class=\"bg-primary text-white p-2\">var_dump :<pre class=\"alert alert-primary w-75\">";
         jeprint_r($mavariable);
         echo "</pre>";
     }
